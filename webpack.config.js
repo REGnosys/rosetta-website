ed611dfb3586803667a3364a9e99f00686700efb
mikebtg <mike@breakthegrid.co.uk>
2021-09-20 13:44:48 +0100

    Adds swiperjs, css-loader and style-loader.

--- webpack.config.js
+++ webpack.config.js
@@ -70,6 +70,10 @@ module.exports = {
                 test: /\.tsx?/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             }
         ]
 
