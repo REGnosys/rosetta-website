8722c7eea71cb28243a6bef56fd9d7879f66e198
Jason Krohn <jason.krohn@regnosys.com>
2021-11-03 11:31:26 +0000

    Update pricing page and minor tweaks

--- server/config.js
+++ server/config.js
@@ -16,10 +16,13 @@ const config = {
         coreDocumentation: "https://docs.rosetta-technology.io/core/",
         linkedIn: "https://www.linkedin.com/company/regnosys",
         twitter: "https://twitter.com/regnosys",
-        domainModel: "https://ellipse.bisih.org/proof-of-concept/demonstration-of-the-mortgage-cdm",
+        domainModel:
+            "https://olegchursin.medium.com/a-brief-introduction-to-domain-modeling-862a30b38353",
         cdmDocs: "https://www.isda.org/2018/11/22/isda-cdm-factsheet/",
         eclipse: "https://www.eclipse.org/modeling/emf/",
-        isda: "https://www.isda.org/2021/10/21/common-domain-model-integrated-into-isda-create/",
+        isda: "https://www.isda.org/2019/10/14/isda-common-domain-model/",
+        isdaCreate:
+            "https://www.isda.org/2021/10/21/common-domain-model-integrated-into-isda-create/",
         isla: "https://www.islaemea.org/regulation-and-policy/digital-fintech/common-domain-model-cdm/",
         icma: "https://www.icmagroup.org/News/news-in-brief/common-domain-model-cdm-for-repo-and-bonds/",
         ethereum:
@@ -30,6 +33,13 @@ const config = {
         regulatoryReporting:
             "https://jwg-it.eu/working-group/global-derivatives-digital-regulatory-reporting-programme/",
         machineExecutable: "https://www.bis.org/press/p201006.htm",
+        dataModelling:
+            "https://ellipse.bisih.org/proof-of-concept/demonstration-of-the-mortgage-cdm",
+    },
+    metrics: {
+        users: 1000,
+        markets: 80,
+        languages: 7,
     },
     pricing: {
         community: {
@@ -58,7 +68,7 @@ const config = {
         },
         pro: {
             yearly: {
-                amount: 1333,
+                amount: 1334,
             },
             monthly: {
                 amount: 1600,
