b3fef2c75491b37ca5daf13b2e233790f88387ea
Jason Krohn <jason.krohn@regnosys.com>
2021-11-05 17:42:36 +0000

    Add cache header to NGINX and change build mode to production

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,12 @@ WORKDIR /usr/src/app
 
 COPY package*.json ./
 
-RUN npm install pm2 -g && \
-    npm ci
-    
+RUN npm i
 COPY . .
 
 RUN npm run compile-sass && \
-    npm run build
+    npm run build -- --mode=production
+RUN npm install pm2 -g
 
 EXPOSE 5000
 CMD ["pm2-runtime", "ecosystem.config.js", "--env production"]
--- webpack.config.js
+++ webpack.config.js
@@ -3,76 +3,89 @@
  */
 
 const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 /**
  *
  */
 
 module.exports = {
-  /**
-   * entry.
-   */
-
-  entry: path.resolve(__dirname, "src/ts/index.ts"),
-
-  /**
-   * output.
-   */
-
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "web/assets/js"),
-  },
-
-  /**
-   * resolve.
-   */
-
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-
-  /**
-   * devtool.
-   */
-
-  devtool: "source-map",
-
-  /**
-   * mode.
-   */
-
-  mode: "development",
-
-  /**
-   * module.
-   */
-
-  module: {
     /**
-     * rules.
+     * entry.
      */
 
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.tsx?/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+    entry: path.resolve(__dirname, "src/ts/index.ts"),
+
+    /**
+     * output.
+     */
+
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "web/assets/js"),
+    },
+
+    /**
+     * resolve.
+     */
+
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+
+    /**
+     * devtool.
+     */
+
+    devtool: "source-map",
+
+    /**
+     * mode.
+     */
+
+    mode: "development",
+
+    /**
+     * module.
+     */
+
+    module: {
+        /**
+         * rules.
+         */
+
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.tsx?/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+            }),
+        ],
+    },
 };
