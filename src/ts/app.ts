b54fbbafa33b79cd131df93e1e7383dc1352b2c9
Jason Krohn <59827242+JasonKrohn-regnosys@users.noreply.github.com>
2022-01-25 10:42:59 +0000

    Task 1848 cookie banner (#29)
    * npm update plus audit fix
    
    * npm update and installed cookie-parser
    
    * Check request cookie and set in config
    
    * Cookie banner component
    
    * Comms agency scripts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,167 +2,163 @@
  * Utils.
  */
 
-import { Ready } from './utils/ready'
+import { Ready } from "./utils/ready";
 
 /**
  * Layouts.
  */
 
-import { Header } from './layouts/common/header'
-import { Footer } from './layouts/common/footer'
-import { MobileNav } from './layouts/common/mobile-nav'
-import { CoreDiagram } from './layouts/pages/governance-core-diagram'
-import { HeaderSubNavigation } from './layouts/common/header-sub-navigation'
-import { PricingTable } from './layouts/pages/pricing-table'
-import { PricingBlocks } from './layouts/pages/pricing-blocks'
+import { Header } from "./layouts/common/header";
+import { Footer } from "./layouts/common/footer";
+import { MobileNav } from "./layouts/common/mobile-nav";
+import { CoreDiagram } from "./layouts/pages/governance-core-diagram";
+import { HeaderSubNavigation } from "./layouts/common/header-sub-navigation";
+import { PricingTable } from "./layouts/pages/pricing-table";
+import { PricingBlocks } from "./layouts/pages/pricing-blocks";
 
 /**
  * Components.
  */
 
-import { LinkWithGraphics } from './components/link-with-graphics'
-import { BodyCurves } from './components/body-curves'
-import { GraphicHeaders } from './components/graphic-headers'
-import { BasicForms } from './components/basic-forms'
-import { ContactForm } from './components/contact-form'
+import { LinkWithGraphics } from "./components/link-with-graphics";
+import { BodyCurves } from "./components/body-curves";
+import { GraphicHeaders } from "./components/graphic-headers";
+import { BasicForms } from "./components/basic-forms";
+import { ContactForm } from "./components/contact-form";
+import { CookieBanner } from "./components/cookie-banner";
 
 /**
  * Effects.
  */
 
-import { AnimatedIcons } from './effects/animated-icons'
-import { ScrollsIntoView } from './effects/scolls-into-view'
-import { NumberCountUp  } from './effects/number-count-up'
-import { LottieAnimations } from './effects/lottie-animations'
-import { MediumZoom } from './effects/medium-zoom'
-import { UrlHash } from './effects/url-hash'
+import { AnimatedIcons } from "./effects/animated-icons";
+import { ScrollsIntoView } from "./effects/scolls-into-view";
+import { NumberCountUp } from "./effects/number-count-up";
+import { LottieAnimations } from "./effects/lottie-animations";
+import { MediumZoom } from "./effects/medium-zoom";
+import { UrlHash } from "./effects/url-hash";
 
 /**
  * App.
  */
 
 export class App {
-
     /**
      * Layouts.
      */
 
-    private header : Header
-    private footer : Footer
-    private mobileNav : MobileNav
-    private coreDiagram : CoreDiagram
-    private headerSubNavigation : HeaderSubNavigation
-    private pricingTable : PricingTable
-    private pricingBlocks : PricingBlocks
+    private header: Header;
+    private footer: Footer;
+    private mobileNav: MobileNav;
+    private coreDiagram: CoreDiagram;
+    private headerSubNavigation: HeaderSubNavigation;
+    private pricingTable: PricingTable;
+    private pricingBlocks: PricingBlocks;
 
     /**
      * Components.
      */
-    
-    private linkWithGraphics : LinkWithGraphics
-    private bodyCurves : BodyCurves
-    private graphicHeaders : GraphicHeaders
-    private basicForms : BasicForms
-    private contactForm : ContactForm
+
+    private linkWithGraphics: LinkWithGraphics;
+    private bodyCurves: BodyCurves;
+    private graphicHeaders: GraphicHeaders;
+    private basicForms: BasicForms;
+    private contactForm: ContactForm;
+    private cookieBanner: CookieBanner;
 
     /**
      * Effects.
      */
 
-    private animatedIcons : AnimatedIcons
-    private scrollsIntoView : ScrollsIntoView
-    private numberCountUp : NumberCountUp
-    private lottieAniations : LottieAnimations
-    private mediumZoom : MediumZoom
-    private urlHash : UrlHash
+    private animatedIcons: AnimatedIcons;
+    private scrollsIntoView: ScrollsIntoView;
+    private numberCountUp: NumberCountUp;
+    private lottieAniations: LottieAnimations;
+    private mediumZoom: MediumZoom;
+    private urlHash: UrlHash;
 
     /**
      * constructor.
      */
 
     constructor() {
-
         /**
          * Layouts.
          */
 
-        this.header = new Header()
-        this.footer = new Footer()
-        this.mobileNav = new MobileNav()
-        this.coreDiagram = new CoreDiagram()
-        this.headerSubNavigation = new HeaderSubNavigation()
-        this.pricingTable = new PricingTable()
-        this.pricingBlocks = new PricingBlocks() 
+        this.header = new Header();
+        this.footer = new Footer();
+        this.mobileNav = new MobileNav();
+        this.coreDiagram = new CoreDiagram();
+        this.headerSubNavigation = new HeaderSubNavigation();
+        this.pricingTable = new PricingTable();
+        this.pricingBlocks = new PricingBlocks();
 
         /**
          * Components.
          */
-        
-        this.linkWithGraphics = new LinkWithGraphics()
-        this.bodyCurves = new BodyCurves()
-        this.graphicHeaders = new GraphicHeaders()
-        this.basicForms = new BasicForms()
-        this.contactForm = new ContactForm()
+
+        this.linkWithGraphics = new LinkWithGraphics();
+        this.bodyCurves = new BodyCurves();
+        this.graphicHeaders = new GraphicHeaders();
+        this.basicForms = new BasicForms();
+        this.contactForm = new ContactForm();
+        this.cookieBanner = new CookieBanner();
 
         /**
          * Effects.
          */
 
-        this.animatedIcons = new AnimatedIcons()
-        this.scrollsIntoView = new ScrollsIntoView()
-        this.numberCountUp = new NumberCountUp()
-        this.lottieAniations = new LottieAnimations()
-        this.mediumZoom = new MediumZoom()
-        this.urlHash = new UrlHash()
-
+        this.animatedIcons = new AnimatedIcons();
+        this.scrollsIntoView = new ScrollsIntoView();
+        this.numberCountUp = new NumberCountUp();
+        this.lottieAniations = new LottieAnimations();
+        this.mediumZoom = new MediumZoom();
+        this.urlHash = new UrlHash();
     }
 
     /**
      * start.
      */
 
-    start(): void {       
-
+    start(): void {
         /**
          * When document is ready.
          */
 
         new Ready(() => {
-
             /**
              * Start layouts.
              */
 
-            this.footer.start()
-            this.mobileNav.start()
-            this.coreDiagram.start()
-            this.headerSubNavigation.start()
-            this.pricingTable.start()
-            this.pricingBlocks.start()
+            this.footer.start();
+            this.mobileNav.start();
+            this.coreDiagram.start();
+            this.headerSubNavigation.start();
+            this.pricingTable.start();
+            this.pricingBlocks.start();
 
             /**
              * Start components.
              */
 
-            this.linkWithGraphics.start()
-            this.bodyCurves.start()
-            this.graphicHeaders.start()
-            this.basicForms.start()
-            this.contactForm.start()
+            this.linkWithGraphics.start();
+            this.bodyCurves.start();
+            this.graphicHeaders.start();
+            this.basicForms.start();
+            this.contactForm.start();
+            this.cookieBanner.start();
 
             /**
              * Start effects.
              */
 
-            this.scrollsIntoView.start()
-            this.numberCountUp.start()
-            this.animatedIcons.start()
-            this.lottieAniations.start()
-            this.mediumZoom.start()
-            this.urlHash.start()
-
-        })
-
+            this.scrollsIntoView.start();
+            this.numberCountUp.start();
+            this.animatedIcons.start();
+            this.lottieAniations.start();
+            this.mediumZoom.start();
+            this.urlHash.start();
+        });
     }
-
 }
